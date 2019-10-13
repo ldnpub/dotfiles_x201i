1cb2eca0ce7d79775f5c4212308081868667cdfc
Rodolphe <ldnpub@hotmail.fr>
2019-10-13 14:08:56 +0200

    Update recover.sh

--- recover.sh
+++ recover.sh
@@ -2,6 +2,11 @@
 
 
 loadkeys fr
+
+mount -o remount,size=2G /run/archiso/cowspace
+pacman -Sy git
+git clone git://github.com/helmuthdu/aui
+git clone git://github.com/ldnpub/dotfiles_x201i
 cryptsetup luksOpen /dev/sda2 btrfs-system
 mount -o subvol=root,ssd /dev/mapper/btrfs-system /mnt
 mount -o subvol=home,ssd /dev/mapper/btrfs-system /mnt/home
